7b4e21a98ca2133cd8a280a53914a1fe03fbde20
nichtmehr99 <nichtmehr99@gmail.com>
2024-03-01 16:51:26 +0100

    error catch

--- app.js
+++ app.js
@@ -1,59 +1,60 @@
 const btnContainer = document.getElementById('btn-container')
 const cardContainer = document.getElementById('card-container')
+const errorContainer = document.getElementById('error-container')
 let selectedCategory = 1000
 const fetchCategories = () => {
     const url = 'https://openapi.programming-hero.com/api/videos/categories'
     fetch(url)
-    .then(res => res.json())
+    .then((res) => res.json())
     .then(({data}) => {
-       data.forEach(card => {
-        console.log(card)
-        const newBtn = document.createElement('button')
-        newBtn.className = 'btn btn-neutral'
-        newBtn.innerText = card.category
-        // to get data by its category
-        newBtn.addEventListener('click', () => fetchDataByCategories(card.category_id))
-        /* ------ */
-        btnContainer.appendChild(newBtn)
-       });
+        data.forEach(card => {
+            const newBtn = document.createElement('button')
+            newBtn.className = 'btn btn-neutral'
+            newBtn.innerText = card.category
+            newBtn.addEventListener('click', () => fetchDataByCategories(card.category_id))
+            btnContainer.appendChild(newBtn)
+
+        });
     })
 }
-//  aita hoilo dynamically category er data gula re show korar jonno
+
 const fetchDataByCategories = (categoryId) => {
     selectedCategory = categoryId
     const url = `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
     fetch(url)
     .then((res) => res.json())
     .then(({data}) => {
-        // must to know to get every time show the data specific for that category
-        cardContainer.innerHTML = ''
-        data.forEach((video) => {
+        if(data.length === 0 ){
+            errorContainer.classList.remove('hidden')
+        }else{
+            errorContainer.classList.add('hidden')
+        }
+        cardContainer.innerText = ''
+        data.forEach((card) => {
             const newCard = document.createElement('div')
             newCard.innerHTML = `
-            <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card w-96 h-96 bg-base-100 shadow-xl">
             <figure>
-              <img src="shape.jpeg" alt="Shoes" />
+              <img src="${card.thumbnail}" alt="Shoes" />
             </figure>
             <div class="flex space-x-5 p-4">
-              <div class="w-14 mt-4">
-                <img class="rounded-full" src="noha.jpeg" alt="" />
+              <div class="w-14 h-14 mt-4">
+                <img class="rounded-full" src="${card.authors[0].profile_picture}" alt="" />
               </div>
               <div>
-                <h2 class="text-xl">Shoes!</h2>
-                <p>If a dog chews</p>
-                <p>Views</p>
+                <h2 class="text-xl">${card.title}</h2>
+                <p>${card.authors[0].profile_name}</p>
+                <p>${card.others.views}</p>
               </div>
             </div>
           </div>
-
             `
-        cardContainer.appendChild(newCard)
+            cardContainer.appendChild(newCard)
+            
         });
     })
+}
 
 
-}  
-
-fetchDataByCategories(selectedCategory)
 fetchCategories()
-
+fetchDataByCategories(selectedCategory)
