1e9debb1f3d0b572438b28156cf6cbd79ff1a920
nichtmehr99 <nichtmehr99@gmail.com>
2024-03-01 10:43:47 +0100

    sob btn ke fetch use kore ansi

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('conencted')
+const btnContainer = document.getElementById('btn-container')
+const fetchCategories = () => {
+    const url = 'https://openapi.programming-hero.com/api/videos/categories'
+    fetch(url)
+    .then(res => res.json())
+    .then(({data}) => {
+       data.forEach(card => {
+        console.log(card)
+        const newBtn = document.createElement('button')
+        newBtn.className = 'btn btn-neutral'
+        newBtn.innerText = card.category
+        btnContainer.appendChild(newBtn)
+       });
+    })
+}
+
+fetchCategories()
+
